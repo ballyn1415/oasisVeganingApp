74fd3df3dfa6f5356c9ebc53869b3dba54919068
ballyn1415 <yn010511@gmail.com>
2023-08-18 18:26:32 +0900

    마이페이지 생성

---
+++ app/src/main/java/com/example/veganing/RegisterActivity.java
@@ -0,0 +1,23 @@
+package com.example.veganing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+
+
+    }
+}
